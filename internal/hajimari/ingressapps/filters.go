61ded2be9997431d7719f98a517de4256301fb93
Tobias Brunner <tobias@tobru.ch>
2021-11-18 07:47:26 +0100

    use ingress v1 api

--- internal/hajimari/ingressapps/filters.go
+++ internal/hajimari/ingressapps/filters.go
@@ -4,11 +4,11 @@ import (
 	"github.com/toboshii/hajimari/internal/annotations"
 	"github.com/toboshii/hajimari/internal/config"
 	"github.com/toboshii/hajimari/internal/util/strings"
-	"k8s.io/api/extensions/v1beta1"
+	v1 "k8s.io/api/networking/v1"
 )
 
 // For filtering ingresses having hajimari enable annotation set to true
-func byHajimariEnableAnnotation(ingress v1beta1.Ingress, appConfig config.Config) bool {
+func byHajimariEnableAnnotation(ingress v1.Ingress, appConfig config.Config) bool {
 	if appConfig.DefaultEnable {
 		if val, ok := ingress.Annotations[annotations.HajimariEnableAnnotation]; ok {
 			// Has Hajimari annotation and is enabled
@@ -29,7 +29,7 @@ func byHajimariEnableAnnotation(ingress v1beta1.Ingress, appConfig config.Config
 }
 
 // For filtering ingresses by hajimari instance
-func byHajimariInstanceAnnotation(ingress v1beta1.Ingress, appConfig config.Config) bool {
+func byHajimariInstanceAnnotation(ingress v1.Ingress, appConfig config.Config) bool {
 	if val, ok := ingress.Annotations[annotations.HajimariInstanceAnnotation]; ok {
 		return strings.ContainsBetweenDelimiter(val, appConfig.InstanceName, ",")
 	}
